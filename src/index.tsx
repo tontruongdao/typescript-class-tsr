0a8acd95c1c4705912344995fffd7b894c15d7df
Truong Dao <ton.truong.dao@outlook.com>
2021-04-19 12:23:51 -0400

    Used functional component

--- src/index.tsx
+++ src/index.tsx
@@ -5,38 +5,36 @@ interface AppProps {
     color?: string; // To make the property optional
 }
 
-interface AppState {
-    counter: number;
+
+// class App extends React.Component<AppProps> {
+
+//     state = { counter: 0 }
+
+
+//     onIncrement = (): void => {
+//         this.setState({ counter: this.state.counter + 1 })
+//     }
+
+//     onDecrement = (): void => {
+//         this.setState({ counter: this.state.counter - 1 })
+//     }
+
+//     render() {
+//         return (
+//             <div>Hi There{this.props.color}
+//                 <button onClick={this.onIncrement}>Increment</button>
+//                 <button onClick={this.onDecrement}>Decrement</button>
+//                 {this.state.counter}
+//             </div>
+//         )
+//     }
+// }
+
+const App = (props: AppProps): JSX.Element => {
+    return (
+        <div>{props.color}</div>
+    )
 }
-
-class App extends React.Component<AppProps, AppState> {
-
-    constructor(props: AppProps) {
-        super(props);
-
-        this.state = { counter: 0 }
-
-    }
-
-    onIncrement = (): void => {
-        this.setState({ counter: this.state.counter + 1 })
-    }
-
-    onDecrement = (): void => {
-        this.setState({ counter: this.state.counter - 1 })
-    }
-
-    render() {
-        return (
-            <div>Hi There{this.props.color}
-                <button onClick={this.onIncrement}>Increment</button>
-                <button onClick={this.onDecrement}>Decrement</button>
-                {this.state.counter}
-            </div>
-        )
-    }
-}
-
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(<App color="red"/>, document.querySelector('#root'))
 
 // Used ```npx create-react-app my-app --template typescript``` to create ts App
