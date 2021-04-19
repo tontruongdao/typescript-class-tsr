6359653b43c429613dbdd33ce83d547e1d33502d
Truong Dao <ton.truong.dao@outlook.com>
2021-04-19 12:08:39 -0400

    Added state to App component

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 interface AppProps {
-    color: string;
-    // color?: string; To make the property optional
+    // color: string;
+    color?: string; // To make the property optional
 }
 
 class App extends React.Component<AppProps> {
+
+    state = { counter: 0 }
+
+    onIncrement = (): void => {
+        this.setState({ counter: this.state.counter + 1 })
+    }
+
+    onDecrement = (): void => {
+        this.setState({ counter: this.state.counter - 1 })
+    }
+
     render() {
-        return <div>Hi There{this.props.color}</div>
+        return (
+            <div>Hi There{this.props.color}
+                <button onClick={this.onIncrement}>Increment</button>
+                <button onClick={this.onDecrement}>Decrement</button>
+                {this.state.counter}
+            </div>
+        )
     }
 }
 
-ReactDOM.render(<App color="red"/>, document.querySelector('#root'))
+ReactDOM.render(<App/>, document.querySelector('#root'))
 
 // Used ```npx create-react-app my-app --template typescript``` to create ts App
