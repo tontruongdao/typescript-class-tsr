c69cc1b389ca88ec5025293ef87a52437db42373
Truong Dao <ton.truong.dao@outlook.com>
2021-04-19 15:37:40 -0400

    Rendered Todo list to App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,8 +10,28 @@ interface AppProps {
 }
 
 class _App extends React.Component<AppProps> {
+
+    // LifeCycle to fetch Data on 3rd party API.
+    // componentDidMount() {
+    //     this.props.fetchTodos();
+    // }
+
+    onButtonClick = (): void => {
+        this.props.fetchTodos();
+    }
+
+    renderList(): JSX.Element[] {
+        return this.props.todos.map((todo: Todo) => {
+            return <div key={todo.id}>{todo.title}</div>
+        })
+    }
+
     render() {
-        return <div>Hi There</div>
+        return (
+        <div>
+            <button onClick={this.onButtonClick}>Fetch</button>
+            {this.renderList()}
+        </div>)
     }
 }
 
