a9d496e595896256aa8fec45c118f7549ec84ea1
Truong Dao <ton.truong.dao@outlook.com>
2021-04-19 14:18:00 -0400

    Initial setup for Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-interface AppProps {
-    color?: string; // To make the property optional
-}
+import { App } from './components/App'
+import { reducers } from './reducers'
 
+const store = createStore(reducers, applyMiddleware(thunk))
 
-// class App extends React.Component<AppProps> {
-
-//     state = { counter: 0 }
-
-
-//     onIncrement = (): void => {
-//         this.setState({ counter: this.state.counter + 1 })
-//     }
-
-//     onDecrement = (): void => {
-//         this.setState({ counter: this.state.counter - 1 })
-//     }
-
-//     render() {
-//         return (
-//             <div>Hi There{this.props.color}
-//                 <button onClick={this.onIncrement}>Increment</button>
-//                 <button onClick={this.onDecrement}>Decrement</button>
-//                 {this.state.counter}
-//             </div>
-//         )
-//     }
-// }
-
-const App = (props: AppProps): JSX.Element => {
-    return (
-        <div>{props.color}</div>
-    )
-}
-ReactDOM.render(<App color="red"/>, document.querySelector('#root'))
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+     document.querySelector('#root'))
 
 // Used ```npx create-react-app my-app --template typescript``` to create ts App
