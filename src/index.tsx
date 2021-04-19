91f3d47041e7bd2baa9939a51d99a54aff3003af
Truong Dao <ton.truong.dao@outlook.com>
2021-04-19 12:00:23 -0400

    Used Generic and interface to enable TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component {
+interface AppProps {
+    color: string;
+    // color?: string; To make the property optional
+}
+
+class App extends React.Component<AppProps> {
     render() {
-        return <div>Hi There</div>
+        return <div>Hi There{this.props.color}</div>
     }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(<App color="red"/>, document.querySelector('#root'))
 
 // Used ```npx create-react-app my-app --template typescript``` to create ts App
